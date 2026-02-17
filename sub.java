74c8db13da966599dea2d1084946cd548b03fc23
Nitin Bisht <nitinbisht652005@gmail.com>
2026-02-17 20:55:56 +0530

    Implement subtraction of two numbers in Main class

---
+++ sub.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main
+{
+
+
+
+public static void main(String args[]){
+Scanner scan = new Scanner(System.in);
+system.out.println("Enter two number");
+int a = scan.nextInt();
+int b = scan.nextInt();
+int c = a - b;
+System.out.println("subtract is: "+ c);
+}
+
+}
